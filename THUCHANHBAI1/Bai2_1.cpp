b0662c721fa0213000b2f82f8ccd59576bdeb2e8
HuynhVanChi <huynhvanchi978@gmail.com>
2024-07-23 22:26:25 +0700

    update

---
+++ THUCHANHBAI1/Bai2_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 20;
+    string fullname = "Huỳnh Văn Khuân.";
+    string country = "Tp.Hồ Chí Minh";
+    float weight = 61;
+    cout << "Tôi tên là : " << fullname << endl;
+    cout<< "Tôi đến từ : " << country << endl;
+    cout << "Năm nay tôi " << age <<" tuổi."<<endl;
+    cout<<"Tôi nặng : " << weight << "kg."<<endl;
+    return 0;
+}
